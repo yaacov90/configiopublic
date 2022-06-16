37182ff4c411541042602fa9fb1f839533392862
admin <admin@configo.com>
2022-06-16 06:05:33 +0000

    v18

--- applicatoin/CONFIGIUS_TEST1/create.sh
+++ applicatoin/CONFIGIUS_TEST1/create.sh
@@ -1,2 +1,2 @@
-kubectl apply -f /home/cius/action_work_dir/CONFIGIUS_TEST1/959285/configius_kube_deployment.yaml
+kubectl apply -f /home/cius/action_work_dir/CONFIGIUS_TEST1/710778/configius_kube_deployment.yaml
 echo PP SSH_CONNECTION_WORK_DIR }}
