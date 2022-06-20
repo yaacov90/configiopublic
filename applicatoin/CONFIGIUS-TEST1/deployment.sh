e4d74e01eb7bba8255e221d5d704187d4b47bbee
admin <admin@configo.com>
2022-06-20 13:29:03 +0000

    v50

--- applicatoin/CONFIGIUS-TEST1/deployment.sh
+++ applicatoin/CONFIGIUS-TEST1/deployment.sh
@@ -1,2 +1,2 @@
-kubectl apply -f /home/cius/action_work_dir/CONFIGIUS_TEST1/configius_kube_deployment.yaml
-echo /home/cius/action_work_dir/CONFIGIUS_TEST1 2
+kubectl apply -f /home/cius/action_work_dir/CONFIGIUS-TEST1/configius_kube_deployment.yaml
+echo /home/cius/action_work_dir/CONFIGIUS-TEST1 2
