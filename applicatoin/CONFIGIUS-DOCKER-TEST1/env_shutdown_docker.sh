58c5741d397479a458a206a7edf00056ac349429
admin <admin@configo.com>
2022-08-13 10:25:28 +0000

    v60

--- applicatoin/CONFIGIUS-DOCKER-TEST1/env_shutdown_docker.sh
+++ applicatoin/CONFIGIUS-DOCKER-TEST1/env_shutdown_docker.sh
@@ -1,3 +1,7 @@
 cd /home/cius2/action_work_dir/CONFIGIUS-DOCKER-TEST1
 docker-compose down
+docker stop configius_fe_1
+docker rm configius_fe_1
+docker stop configius_be_1
+docker rm configius_be_1
 cd -
