57cebc16761bd5a70084d81a7969de7e6465199f
admin <admin@configo.com>
2022-09-23 20:57:36 +0200

    v68

--- applicatoin/TEST4/deployment.sh
+++ applicatoin/TEST4/deployment.sh
@@ -1,2 +1,2 @@
-kubectl apply -f /home/cius2/action_work_dir/TEST4/configius_kube_deployment.yaml
-echo /home/cius2/action_work_dir/TEST4 2
+kubectl apply -f /home/cius2/action-work-dir/TEST4/configius_kube_deployment.yaml
+echo /home/cius2/action-work-dir/TEST4 2
